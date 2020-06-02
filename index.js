e6ecd51c3de23390a517093df5787e2c770251e5
repl.it user <replituser@example.com>
2020-06-02 14:04:36 +0000

    this is for u samar <3

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ const Staff = require('./model/staff.js');
 const Student = require('./model/student.js');
 const Teacher  =  require('./model/teacher.js');
 
-
-
-const samar = new Teacher("10" , "grade9" , "english class" , "bla bal")
-const rania = new Staff("rania","obied","81671637","beirut","rania@codi.tech","20000$","sdjanj","jijaiojd","ojeijdi");
-
-
-cosole.log ("samar")
+console.log("Samar we love you");
